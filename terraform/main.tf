416de3e6c945a8b48ae31f457ccb5f7205bfebdd
Hayvid <davidhyppolitedh@gmail.com>
2025-01-06 18:47:15 -0500

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,21 +14,20 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source       = "./modules/vpc"
   project_name = var.project_name
 
 }
 
 module "security_group" {
-  source = "./modules/security-group"
+  source       = "./modules/security-group"
   project_name = var.project_name
-  vpc_id = module.vpc.vpc_id
+  vpc_id       = module.vpc.vpc_id
 }
 
 module "eks" {
-  source = "./modules/eks"
+  source             = "./modules/eks"
   private_subnet_ids = module.vpc.private_subnet_ids
-  eks_sg_id = module.security_group.eks_sg_id
+  eks_sg_id          = module.security_group.eks_sg_id
 }
-
 
