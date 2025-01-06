4611e39b4d8e84512db384b1ab18b098a1e5c866
Hayvid <davidhyppolitedh@gmail.com>
2025-01-05 22:34:08 -0500

    update to include module reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,23 +9,26 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
   profile = var.aws_profile
 }
 
 module "vpc" {
   source = "./modules/vpc"
-
+  project_name = var.project_name
 
 }
 
 module "security_group" {
   source = "./modules/security-group"
+  project_name = var.project_name
+  vpc_id = module.vpc.vpc_id
 }
 
 module "eks" {
   source = "./modules/eks"
-
+  private_subnet_ids = module.vpc.private_subnet_ids
+  eks_sg_id = module.security_group.eks_sg_id
 }
 
 
