8ccd0dffa4cf09ab2f80a7ba3d7ea3370e532403
Hayvid <davidhyppolitedh@gmail.com>
2025-01-07 21:53:37 -0500

    update to add comments

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,13 +1,17 @@
 variable "aws_profile" {
+  description = "Specifies the AWS CLI profile to use for Terraform operations." 
+  # use export AWS_PROFILE=backup
   type    = string
-  default = " "
+  default = " " # Default to the AWS CLI default profile. Users can override as needed.
 }
 
 variable "aws_region" {
+  description = "AWS region to deploy resources."
   type = string
 }
 
 variable "project_name" {
   type    = string
   default = "fastAPI-kube-app"
+  description = "Name of the project."
 }
